d200a854b4f60d5cdb908b4182a1884af3052698
Dr. XyClo <sdcarmody@gmail.com>
2019-01-09 15:21:13 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -98,7 +98,6 @@ def changing_alan
         },
         :alan_kay => {
           :known_for => "Object Orientation",
-          
           :languages => ["Smalltalk", "LISP"]
         },
         :dennis_ritchie => {
@@ -107,15 +106,16 @@ def changing_alan
         }
           
      }
-    
+
     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
       alans_new_info = "GUI"
        #  1==1
-     programmer_hash[:alan_kay][:known_for] = "GUI" 
-   
+       # puts programmer_hash[:alan_kay][:known_for] 
+     programmer_hash[:alan_kay][:known_for] = "GUI"
      
+     return programmer_hash
 end
-
+changing_alan
 def adding_to_dennis
 	programmer_hash = 
  		{
@@ -133,6 +133,7 @@ def adding_to_dennis
         }
      }
  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  return programmer_hash
 #binding.pry 
 #1==1
 end
