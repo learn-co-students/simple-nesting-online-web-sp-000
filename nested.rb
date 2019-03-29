ab925abcb0174c18bd973bdca6951b28350420ee
Lauren McCoy <lmccoy8911@gmail.com>
2019-03-29 20:02:05 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -73,7 +73,6 @@ def adding_matz
  		      :known_for => "Ruby",
  		      :languages => ["LISP", "C"]
  		    },
- 		  
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -109,7 +108,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+     programmer_hash
      
 end
 
@@ -129,5 +129,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+    programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+    programmer_hash
 end
