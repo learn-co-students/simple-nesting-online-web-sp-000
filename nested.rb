f033e022a9c59429c75562a65b9a459a007023ed
github username <github email address>
2019-05-21 10:56:45 -0700

    Done.

--- nested.rb
+++ nested.rb
@@ -16,13 +16,15 @@ def hopper
         }
      }
 
+     programmer_hash[:grace_hopper] 
+    
+  end
 
-end
+  hopper
+     
 
-def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
-	programmer_hash = 
+def alan_kay_is_known_for	
+  programmer_hash = 
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -37,7 +39,13 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+
+     programmer_hash[:alan_kay][:known_for]
+
 end
+
+alan_kay_is_known_for
+
 
 def dennis_ritchies_language
 	programmer_hash = 
@@ -55,14 +63,15 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+
+     programmer_hash[:dennis_ritchie][:languages].join("")
+
 end
 
+dennis_ritchies_language
+
+
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
 
 	programmer_hash = 
  		{
@@ -78,10 +87,20 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         }
+
+
      }
 
-    
+     programmer_hash = 
+     {:yukihiro_matsumoto => {
+         :known_for => "Ruby",
+          :languages => ["LISP", "C"]}
+    }
+
 end
+
+adding_matz
+
 
 def changing_alan
 	programmer_hash = 
@@ -99,11 +118,14 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
      
-     
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+     programmer_hash
+        
 end
+
+changing_alan
+
 
 def adding_to_dennis
 	programmer_hash = 
@@ -122,4 +144,9 @@ def adding_to_dennis
         }
      }
 
+     programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+     programmer_hash
+
 end
+
+adding_to_dennis
