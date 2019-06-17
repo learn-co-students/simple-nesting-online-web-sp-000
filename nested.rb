faa483087635759294dab38b3c9f22430a19dfb2
Connor Cowan <ccowan003@comcast.net>
2019-06-17 23:02:43 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,6 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
+		 return programmer_hash[:grace_hopper]
 
 end
 
@@ -35,9 +36,8 @@ def alan_kay_is_known_for
           :known_for => "Unix",
           :languages => ["C"]
         }
-				programmer_hash
      }
-		 programmer_hash
+		 return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -56,6 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+		 return programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -79,8 +80,11 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
-
+     }						#This here adds new key to top level of the hash
+		 							programmer_hash[:yukihiro_matsumoto] = {}
+									programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+									programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+									return programmer_hash
 
 end
 
@@ -103,6 +107,9 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
 
+		 programmer_hash[:alan_kay][:known_for] = alans_new_info
+		 return programmer_hash
+
 
 end
 
@@ -122,5 +129,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+		 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+		 return programmer_hash
 end
