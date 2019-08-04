ea705758abb281d7df8b5ea2a4f650ca67b960fc
Christopher Dent <christopherdent01@gmail.com>
2019-08-04 02:59:46 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+return programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,7 +56,13 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+
+  array = programmer_hash[:dennis_ritchie][:languages]
+    array.join
+
 end
+
+
 
 def adding_matz
 # add the following information to the top level of programmer_hash
@@ -65,6 +72,7 @@ def adding_matz
 # }
 
 	programmer_hash = 
+	
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -79,9 +87,14 @@ def adding_matz
           :languages => ["C"]
         }
      }
+     
+programmer_hash[:yukihiro_matsumoto]={:known_for => "Ruby", :languages => ["C"]}
 
+return programmer_hash
     
 end
+
+
 
 def changing_alan
 	programmer_hash = 
@@ -102,8 +115,12 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+return programmer_hash
 end
+
+
+
 
 def adding_to_dennis
 	programmer_hash = 
@@ -122,4 +139,8 @@ def adding_to_dennis
         }
      }
 
+
+      programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+return programmer_hash
 end
+adding_to_dennis
