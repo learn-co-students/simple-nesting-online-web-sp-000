67ad64923587980c51aab7e04ceedd60c994adcd
Richard Lee <richlee81192@gmail.com>
2019-09-24 21:35:23 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -83,7 +83,7 @@ def adding_matz
      }
 
     programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
-    
+    programmer_hash
 end
 
 def changing_alan
@@ -106,7 +106,7 @@ def changing_alan
      alans_new_info = "GUI"
      
      programmer_hash[:alan_kay][:known_for] = alans_new_info
-     
+     programmer_hash
 end
 
 def adding_to_dennis
@@ -126,4 +126,5 @@ def adding_to_dennis
         }
      }
   programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  programmer_hash
 end
