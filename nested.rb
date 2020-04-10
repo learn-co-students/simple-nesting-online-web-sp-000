cecc413392a05bbc42fcfeed8bea40e4c603bb10
santiago molinari <santomolinari@gmail.com>
2020-04-10 16:56:26 +0200

    Done.

--- nested.rb
+++ nested.rb
@@ -1,7 +1,6 @@
 
 def hopper
-	programmer_hash = 
- 		{
+	programmer_hash = {
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -16,7 +15,9 @@ def hopper
         }
      }
 
+programmer_hash[:grace_hopper]
 
+# programmer_hash[:dennis_ritchie][:languages].to_s
 end
 
 def alan_kay_is_known_for
@@ -37,6 +38,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +58,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+
+programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -64,8 +69,11 @@ def adding_matz
 #    :languages => ["LISP", "C"]
 # }
 
-	programmer_hash = 
- 		{
+	programmer_hash = {   
+      :yukihiro_matsumoto => {
+      :known_for => "Ruby",
+       :languages => ["LISP", "C"]
+        },
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -80,7 +88,7 @@ def adding_matz
         }
      }
 
-    
+programmer_hash
 end
 
 def changing_alan
@@ -100,7 +108,9 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
+ 
+programmer_hash[:alan_kay][:known_for] = "GUI"   
+programmer_hash
      
      
 end
@@ -121,5 +131,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+programmer_hash
 
 end
