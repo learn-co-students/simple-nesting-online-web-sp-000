6ea3050b75437b7c8912773908b1ed8dcf9bfe27
reevesalic <reevesalic@gmail.com>
2020-06-20 23:03:51 -0400

    Done.

--- nested.rb
+++ nested.rb
@@ -57,6 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -79,10 +80,12 @@ def adding_matz
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
-        }
-     }
-
-    
+      }  
+     
+    }
+     programmer_hash.merge(:yukihiro_matsumoto => {:known_for => "Ruby",
+      :languages => ["LISP", "C"]})
+     
 end
 
 def changing_alan
@@ -103,8 +106,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
-     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     	    programmer_hash
 end
 
 def adding_to_dennis
@@ -123,5 +126,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
+programmer_hash
 end
