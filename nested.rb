7518116f646ba7dbe38361fb25550e23081e165d
javonesm89 <javosmith12@gmail.com>
2020-08-31 10:13:17 -0400

    Done.

--- nested.rb
+++ nested.rb
@@ -16,6 +16,8 @@ def hopper
         }
      }
 
+     programmer_hash[:grace_hopper]
+
 
 end
 
@@ -37,6 +39,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +59,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+
+     programmer_hash[:dennis_ritchie][:languages].join('')
 end
 
 def adding_matz
@@ -64,21 +70,25 @@ def adding_matz
 #    :languages => ["LISP", "C"]
 # }
 
-	programmer_hash = 
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
+  programmer_hash = {
+    :yukihiro_matsumoto => {
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]
+  }, 
+    :grace_hopper => {
+      :known_for => "COBOL",
+      :languages => ["COBOL", "FORTRAN"]
+  }, 
+  :alan_kay => {
+    :known_for => "Object Orientation",
+    :languages => ["Smalltalk", "LISP"]
+  },
+  :dennis_ritchie => {
+    :known_for => "Unix",
+    :languages => ["C"]
+  }
+
+  }
 
     
 end
@@ -99,10 +109,11 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      
-     
+     programmer_hash
 end
 
 def adding_to_dennis
@@ -121,5 +132,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+
+     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+     programmer_hash
 
 end
