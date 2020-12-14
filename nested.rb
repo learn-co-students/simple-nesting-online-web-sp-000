d1db72f8219254bebdf1f39a761f2030fe96cd0c
HELEN HARMAN <hlharman@bellsouth.net>
2020-12-14 19:18:02 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -124,20 +124,29 @@ def changing_alan
       
      
 programmer_hash[:alan_kay][:known_for] = "GUI"
-programmer_hash[:alan_kay]
+programmer_hash
 end 
  
 
 def adding_to_dennis
 	programmer_hash = 
-	{
-   :dennis_ritchie => {
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
-  }
-	}  
+        }
+     }
+	  
 	programmer_hash[:dennis_ritchie][:languages] = "C", "Assembly"
-  programmer_hash[:dennis_ritchie]
+  programmer_hash
 
 
- end 
+ end
