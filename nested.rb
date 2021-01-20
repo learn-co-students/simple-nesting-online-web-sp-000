d67e437bb09bbb1d75e401757c523f64d29fc202
Austin Hernandez <ahernandez1170@gmail.com>
2021-01-20 22:48:33 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,12 +15,11 @@ def hopper
           :languages => ["C"]
         }
      }
-
+hopper = programmer_hash[:grace_hopper]
 
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
 	programmer_hash = 
  		{
@@ -37,11 +36,19 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+programmer_hash[:alan_kay][:known_for]
+
+
 end
 
 def dennis_ritchies_language
 	programmer_hash = 
- 		{
+ 	  {
+        :yukihiro_matsumoto => {
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]
+        },
+       
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -55,17 +62,19 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+          array = programmer_hash[:dennis_ritchie][:languages]
+		      array.join
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+# add the following information to the top level of programmer_hash 
 
 	programmer_hash = 
  		{
+        :yukihiro_matsumoto => {
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]
+        },
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -80,7 +89,7 @@ def adding_matz
         }
      }
 
-    
+  
 end
 
 def changing_alan
@@ -100,10 +109,13 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
      
-     
+   alans_new_info = "GUI"
+   programmer_hash[:alan_kay][:known_for] = alans_new_info
+   programmer_hash
 end
+
+
 
 def adding_to_dennis
 	programmer_hash = 
@@ -122,4 +134,6 @@ def adding_to_dennis
         }
      }
 
+    programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+    programmer_hash
 end
