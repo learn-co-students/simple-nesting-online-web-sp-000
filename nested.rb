89a31b224ae7a0ea1f64df05d2492bc8c97195f2
authentic-pipe-8414 <tasher.stidham@outlook.com>
2021-01-20 20:53:36 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+return programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+ languages = programmer_hash[:dennis_ritchie][:languages]
+ return languages[0]
 end
 
 def adding_matz
@@ -78,8 +81,10 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
+     } 
+programmer_hash[:yukihiro_matsumoto] = {}
 
+return programmer_hash
     
 end
 
@@ -100,7 +105,8 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+    return programmer_hash
      
      
 end
@@ -121,5 +127,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+return programmer_hash
 
 end
